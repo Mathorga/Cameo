28477adc1a50d92587467140ed77f0b1ab39995f
Luka Micheletti <l.micheletti@ebw.it>
2025-07-17 12:46:51 +0200

    Removed package.json

--- start_stream.sh
+++ start_stream.sh
@@ -1,2 +1,3 @@
 # LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -vf -hf -fps 15 -q 50 -x 640 -y 480" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
 rpicam-vid -t 0 --inline --listen -o tcp://127.0.0.1:9000
+# rpicam-vid -t 0 -o - | nc -l -p 9000
