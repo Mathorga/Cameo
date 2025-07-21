e225fa5f741f840aa1e7bf6a51b35c17c74dbcc5
Luka Micheletti <l.micheletti@ebw.it>
2025-07-21 17:19:30 +0200

    Removed python stuff

--- cameo_server.js
+++ cameo_server.js
@@ -7,7 +7,7 @@ var ads1x15 = require("node-ads1x15");
 var adc = new ads1x15(1); // set to 0 for ads1015
 
 // var Gpio = require("pigpio").Gpio,
-//   A1 = new Gpio(27, {mode: Gpio.OUTPUT}),
+//   A1 = new G pio(27, {mode: Gpio.OUTPUT}),
 //   A2 = new Gpio(17, {mode: Gpio.OUTPUT}),
 //   B1 = new Gpio( 4, {mode: Gpio.OUTPUT}),
 //   B2 = new Gpio(18, {mode: Gpio.OUTPUT});
@@ -21,43 +21,17 @@ app.get(
   }
 );
 
-child = exec(
-  "sudo bash start_stream.sh",
-  function (error, stdout, stderr) { }
-);
+// Start the streaming process.
+// child = exec(
+//   "sudo bash start_stream.sh",
+//   function (error, stdout, stderr) { }
+// );
 
 //Whenever someone connects this gets executed
 io.on(
   "connection",
   function (socket) {
     console.log("A user connected");
-
-    // socket.on(
-    //   "pos",
-    //   function (msx, msy) {
-    //     //console.log("X:" + msx + " Y: " + msy);
-    //     //io.emit("posBack", msx, msy);
-
-    //     msx = Math.min(Math.max(parseInt(msx), -255), 255);
-    //     msy = Math.min(Math.max(parseInt(msy), -255), 255);
-
-    //     if (msx > 0) {
-    //       A1.pwmWrite(msx);
-    //       A2.pwmWrite(0);
-    //     } else {
-    //       A1.pwmWrite(0);
-    //       A2.pwmWrite(Math.abs(msx));
-    //     }
-
-    //     if (msy > 0) {
-    //       B1.pwmWrite(msy);
-    //       B2.pwmWrite(0);
-    //     } else {
-    //       B1.pwmWrite(0);
-    //       B2.pwmWrite(Math.abs(msy));
-    //     }
-    //   }
-    // );
 
     // socket.on(
     //   "light",
@@ -103,15 +77,18 @@ io.on(
 
     setInterval(
       function () { // send temperature every 5 sec
-        child = exec("cat /sys/class/thermal/thermal_zone0/temp", function (error, stdout, stderr) {
-          if (error !== null) {
-            console.log("exec error: " + error);
-          } else {
-            var temp = parseFloat(stdout) / 1000;
-            io.emit("temp", temp);
-            console.log("temp", temp);
+        child = exec(
+          "cat /sys/class/thermal/thermal_zone0/temp",
+          function (error, stdout, stderr) {
+            if (error !== null) {
+              console.log("exec error: " + error);
+            } else {
+              var temp = parseFloat(stdout) / 1000;
+              io.emit("temp", temp);
+              console.log("temp", temp);
+            }
           }
-        });
+        );
         // if (!adc.busy) {
         //   adc.readADCSingleEnded(0, "4096", "250", function (err, data) { //channel, gain, samples
         //     if (!err) {
--- python.md
+++
@@ -1,5 +0,0 @@
-```
-sudo apt update
-sudo apt install python3-pip ffmpeg libatlas-base-dev libcamera-apps
-pip3 install aiortc aiohttp av numpy
-```
