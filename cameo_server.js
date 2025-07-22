4e22b88f94307856fdd381e738f25e6dbc3cceb3
Luka Micheletti <l.micheletti@ebw.it>
2025-07-22 13:43:06 +0200

    Updated client and server

--- cameo_server.js
+++ cameo_server.js
@@ -89,15 +89,15 @@ io.on(
             }
           }
         );
-        if (!adc.busy) {
-          adc.readADCSingleEnded(0, "4096", "250", function (err, data) { //channel, gain, samples
-            if (!err) {
-              voltage = 2 * parseFloat(data) / 1000;
-              console.log("ADC: ", voltage);
-              io.emit("volt", voltage);
-            }
-          });
-        }
+        // if (!adc.busy) {
+        //   adc.readADCSingleEnded(0, "4096", "250", function (err, data) { //channel, gain, samples
+        //     if (!err) {
+        //       voltage = 2 * parseFloat(data) / 1000;
+        //       console.log("ADC: ", voltage);
+        //       io.emit("volt", voltage);
+        //     }
+        //   });
+        // }
       },
       5000
     );
