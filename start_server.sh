e972cbd904dc34a26d9c79d5f66861d5c7aa1945
Luka Micheletti <l.micheletti@ebw.it>
2025-07-23 14:44:37 +0200

    Updated start script

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Start streaming from cam.
-~/Cameo/start_stream.sh
-/usr/bin/node ~/Cameo/cameo_server.js &
+LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_libcamera.so -r 640x480 -fps 30" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www"
+/usr/bin/node /home/pi/Cameo/cameo_server.js
