73e45db24d04262c625528c429df814fb7137459
Luka Micheletti <l.micheletti@ebw.it>
2025-07-23 17:11:44 +0200

    Updated server

--- cameo_server.js
+++ cameo_server.js
@@ -36,7 +36,8 @@ io.on(
         child = exec("find -type f -name \"*.jpg\" | wc -l", function (error, stdout, stderr) {
           numPics = parseInt(stdout) + 1;
           // Turn off streamer, take photo, restart streamer
-          var command = "sudo killall mjpg_streamer ; rpicam-jpeg -t 2000 -o cam" + numPics + ".jpg -n && sudo bash start_stream.sh";
+          // const command = "sudo killall mjpg_streamer ; rpicam-jpeg -t 2000 -o cam" + numPics + ".jpg -n && sudo bash start_stream.sh";
+          const command = "sudo systemctl stop cameo_stream.service ; rpicam-jpeg -t 2000 -o cam" + numPics + ".jpg -n && sudo systemctl start cameo_stream.service";
           child = exec(command, function (error, stdout, stderr) {
             console.log("Picture taken: cam" + numPics + ".jpg");
             io.emit("cam", 1);
