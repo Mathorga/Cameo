c27f3bff9fbb9e9585caf18a843bd1c8e128e6fe
Luka Micheletti <l.micheletti@ebw.it>
2025-07-23 12:48:24 +0200

    Updated service

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Start streaming from cam.
 ~/Cameo/start_stream.sh
 /usr/bin/node ~/Cameo/cameo_server.js &
--- start_stream.sh
+++ start_stream.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Launch mjpg-streamer with libcamera input and http output. All outputs are suppressed.
 # Remove [... > /dev/null 2>&1&] at the end to display outputs.
 LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_libcamera.so -r 640x480 -fps 30" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
