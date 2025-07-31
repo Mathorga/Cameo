bc0c97bbb701bb935e4c5b8ae9933b2ecbe5f4d8
Luka Micheletti <l.micheletti@ebw.it>
2025-07-31 13:34:09 +0200

    updated start_server script

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start streaming from cam.
-/home/pi/Cameo/start_stream.sh
+sudo /home/pi/Cameo/start_stream.sh
 
 # Start http server.
-/usr/bin/node /home/pi/Cameo/cameo_server.js
+sudo /usr/bin/node /home/pi/Cameo/cameo_server.js
