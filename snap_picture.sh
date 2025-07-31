690775be57529075b8693f288b9cba7d35c3a1d9
Luka Micheletti <l.micheletti@ebw.it>
2025-07-31 17:57:31 +0200

    Updated snap_picture script

--- snap_picture.sh
+++ snap_picture.sh
@@ -8,7 +8,7 @@
 LOGFILE=$(mktemp)
 
 # Start mjpg-streamer in background and redirect output
-LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_libcamera.so -r 640x480 -fps 30" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
+LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_libcamera.so -r 640x480 -fps 30" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www" > $LOGFILE 2>&1&
 MJPG_PID=$!
 
 # Wait for a known "ready" message
