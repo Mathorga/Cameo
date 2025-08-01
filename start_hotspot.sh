8b6732c4197ec9f6148c46f3b243ed3b5646f290
Luka Micheletti <l.micheletti@ebw.it>
2025-08-01 16:14:12 +0200

    Updated hotspot setup script

--- start_hotspot.sh
+++ start_hotspot.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Create the hotspot.
 /usr/bin/nmcli connection add type wifi ifname wlan0 con-name cameo-hotspot autoconnect yes ssid cameo
 /usr/bin/nmcli connection modify cameo-hotspot 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
 /usr/bin/nmcli connection modify cameo-hotspot wifi-sec.key-mgmt wpa-psk
 /usr/bin/nmcli connection modify cameo-hotspot wifi-sec.psk "raspberry"
+
+# Start the hotspot.
 /usr/bin/nmcli connection up cameo-hotspot
